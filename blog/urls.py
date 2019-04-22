5b33f297e1e7d50bf3ed94acfa140e276b0673bc
skuanyshuly <chika9507@gmail.com>
2019-04-22 21:11:49 +0600

    templates added

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,12 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-path('', views.post_list, name='post_list'),
+path('', views.index, name='index'),
+path('series', views.series, name='series'),
+path('free_episodes', views.free_episodes, name='free_episodes'),
+path('gethbo', views.gethbo, name='gethbo'),
+
+
 	# path('main', views.main, name='main'),
 
 
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,19 @@ def post_list(request):
 	posts = Post.objects.all()
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
+def series(request):
+	return render(request, 'blog/series.html')
+
+def free_episodes(request):
+	return render(request, 'blog/free_episodes.html')
+
+def gethbo(request):
+	return render(request, 'blog/gethbo.html')
+
+def index(request):
+	return render(request, 'blog/index.html')
+
+
+
+
+
